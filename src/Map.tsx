16e869eb40694b759a364c57fb7b7eb76a631262
Jim Lawruk <jimlawruk@yahoo.com>
2025-03-07 07:27:11 -0500

    Add Monuments and Plans layers to map.

--- src/Map.tsx
+++ src/Map.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect } from "react";
 import EsriMap from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 function Map() {
   useEffect(() => {
@@ -12,9 +13,21 @@ function Map() {
     // @ts-ignore
     const view = new MapView({
       map: map,
-      center: [-77, 39],
-      zoom: 7,
+      center: [-82.5, 40],
+      zoom: 8,
       container: "mapDiv",
+    });
+
+    view.when(() => {
+      const monuments = new FeatureLayer({
+        url: "https://odotgis.dot.state.oh.us/arcgis/rest/services/OhROW/ROW_Monuments_Viewer/MapServer/0",
+      });
+      map.add(monuments);
+      const plans = new FeatureLayer({
+        url: "https://odotgis.dot.state.oh.us/arcgis/rest/services/OhROW/ROW_Monuments_Viewer/MapServer/1",
+      });
+      map.add(monuments);
+      map.add(plans);
     });
   }, []);
 
